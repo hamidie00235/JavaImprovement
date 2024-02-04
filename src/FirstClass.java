042619ca98798b8ccc011b8139d9cf6930683d13
hamidie00235 <ahmathisseinh@gmail.com>
2024-02-05 02:40:37 +0300

    if-then Statement

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,8 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello Worl!");
+        System.out.println("Hello Worl!");
+        boolean isAlien =false;
+        if(isAlien==false){
+            System.out.println("it is not an Alien");}
     }
 }
