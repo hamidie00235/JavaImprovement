5c4be69ae31987244361b939eaef60ebd4030bca
hamidie00235 <ahmathisseinh@gmail.com>
2024-02-06 14:55:53 +0300

    Logical AND Operator

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,8 +1,20 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello Worl!");
-        boolean isAlien =false;
-        if(isAlien==false){
-            System.out.println("it is not an Alien");}
+        System.out.println("Hello World!");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien");
+        }
+
+        int topScore = 60;
+        if (topScore < 100) {
+            System.out.println("You got the high score");
+        }
+        int secondTopScore=39;
+        if(topScore>secondTopScore && topScore<100){
+            System.out.println("you got greater than secondTopScore and less than 100");
+
+        }
     }
 }
+
