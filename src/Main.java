c2d5a4515448771e7548e83d5d41fdf91355327f
hamidie00235 <ahmathisseinh@gmail.com>
2024-02-10 14:01:43 +0300

    Code Blocks And The If Then Else Control Statement

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         //Code Blocks And The If Then Else Control Statement
         boolean gameOver = true;
-        int score =5000;
+        int score =500;
         int levelCompleted =5;
         int bonus =100;
 
         if(score<3000 && score >100){
             System.out.println("Your score was less than 300 and greater than 100");
         }
-        else if(score>6000){
+        else if(score>600){
             System.out.println("the score was less than 600");
         }
      else {
