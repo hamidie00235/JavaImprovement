b07758e9044959797edfafacc0f3f0e6676310b2
hamidie00235 <ahmathisseinh@gmail.com>
2024-02-17 16:11:44 +0300

    Exercice de codage 2:speed converter

--- src/CodingExercises.java
+++ src/CodingExercises.java
@@ -1,20 +1,56 @@
 public class CodingExercises {
+
     public static void main(String[] args) {
-        checkNumber(0);
+
+
+
+         CodingExercises speedConverter =new CodingExercises();
+ speedConverter.printConversion(1.5);
+ speedConverter.printConversion(10.25);
+        speedConverter.printConversion(-5.6);
+        speedConverter.printConversion(25.42);
+        speedConverter.printConversion(75.114);
+
+
+//        checkNumber(0);
+//
+//    }
+//    // Exercice de codage 1 : Positive, Negative or Zero
+//    public static void checkNumber(int number){
+//
+//        if(number>0){
+//            System.out.println("positive");
+//        }
+//        else if(number<0){
+//            System.out.println("negative");
+//        }
+//       else{
+//            System.out.println("zero");
+//        }
+    }
+        // Exercice de codage 2:speed converter
+        public static long toMilesPerHour(double kilometersPerHour){
+
+            // invalid option part
+            if (kilometersPerHour<0){
+                return -1;
+            }
+           // convert the value from kilometers to miles per hour
+           long miles =Math.round(kilometersPerHour/1.609);
+           return miles;
+
 
     }
-    // Exercice de codage 1 : Positive, Negative or Zero
-    public static void checkNumber(int number){
+      public static void printConversion(double kilometersPerHour){
+        // invalid option
+          if(kilometersPerHour<0){
+              System.out.println("Invalid value");
+              return;
+          }
 
-        if(number>0){
-            System.out.println("positive");
-        }
-        else if(number<0){
-            System.out.println("negative");
-        }
-       else{
-            System.out.println("zero");
-        }
-    }
+          // calculate from kilometres to miles per hour
+          long conversion =toMilesPerHour(kilometersPerHour);
+          System.out.println(kilometersPerHour + " km/h = " + conversion + " mi/h ");
+      }
 }
 
